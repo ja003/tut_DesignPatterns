3589d4f62ce9b302a221c97d3454767858868bd5
Adam Jurík <ja004@seznam.cz>
2020-05-13 16:37:21 +0200

    Init

---
+++ 3. Builder/Exercise_Builder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tut_DesignPatterns._3._Builder
+{
+	public class Exercise_Builder
+	{
+		//Console.WriteLine();
+
+		public static void Start()
+		{
+			Console.WriteLine("Exercise_Builder");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tut_DesignPatterns._3._Builder;
+
+namespace tut_DesignPatterns
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Main");
+
+			Exercise_Builder.Start();
+
+			Console.ReadKey();
+		}
+	}
+}
