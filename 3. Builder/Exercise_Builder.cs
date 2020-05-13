abbc3619b7515cca8d2aecbe0c61a6193fc2bcd3
Adam Jurík <ja004@seznam.cz>
2020-05-13 16:54:57 +0200

    my try

--- 3. Builder/Exercise_Builder.cs
+++ 3. Builder/Exercise_Builder.cs
@@ -4,15 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tut_DesignPatterns._3._Builder
-{
-	public class Exercise_Builder
-	{
-		//Console.WriteLine();
 
-		public static void Start()
+
+public class CodeBuilder
+{
+	string className;
+	List<Attribute> attributes = new List<Attribute>();
+
+	public CodeBuilder(string pClass)
+	{
+		className = pClass;
+	}
+
+	public CodeBuilder AddField(string pName, string pType)
+	{
+		attributes.Add(new Attribute() { name = pName, type = pType });
+		return this;
+	}
+
+	public override string ToString()
+	{
+		return $"public class {className}{Environment.NewLine}" +
+			'{' + Environment.NewLine +
+				attributes[0].ToString() +
+			'}' + Environment.NewLine
+			;
+	}
+
+	private class Attribute
+	{
+		public string type;
+		public string name;
+
+		public override string ToString()
 		{
-			Console.WriteLine("Exercise_Builder");
+			return $"  public {type} {name};{Environment.NewLine}";
 		}
+	}
+}
+
+public class Exercise_Builder
+{
+	//Console.WriteLine();
+
+	public static void Start()
+	{
+		Console.WriteLine("Exercise_Builder");
+
+		var cb = new CodeBuilder("Person")
+			.AddField("Name", "string")
+			.AddField("Age", "int");
+		Console.WriteLine(cb);
 	}
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using tut_DesignPatterns._3._Builder;
 
 namespace tut_DesignPatterns
 {
