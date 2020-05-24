8478bfcea55cf7f662a0ee9cd2dd2aa4d331a9f8
Adam Jurík <ja004@seznam.cz>
2020-05-24 17:53:27 +0200

    5. Singleton

---
+++ Exercise_Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tut_DesignPatterns
+{
+    class Exercise_Singleton
+    {
+        public static void Start()
+        {
+        }
+    }
+}
+
+
+namespace Coding.Exercise
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            // todo
+            object obj1 = func();
+            object obj2 = func();
+
+            return obj1.Equals(obj2);
+        }
+
+        public static bool IsSingleton_solution(Func<object> func)
+        {
+            var obj1 = func();
+            var obj2 = func();
+            return ReferenceEquals(obj1, obj2);
+        }
+    }
+}
