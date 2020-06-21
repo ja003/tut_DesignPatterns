88166fce26589c5c574788efdf692b0b02afdb83
Adam Jurík <ja004@seznam.cz>
2020-06-21 10:34:40 +0200

    6. Adapter

---
+++ Exercise_Adapter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tut_DesignPatterns
+{
+	class Exercise_Adapter
+	{
+		public static void Start()
+		{
+		}
+	}
+}
+
+
+namespace Coding.Exercise
+{
+	public class Square
+	{
+		public int Side;
+	}
+
+	public interface IRectangle
+	{
+		int Width { get; }
+		int Height { get; }
+	}
+
+	public static class ExtensionMethods
+	{
+		public static int Area(this IRectangle rc)
+		{
+			return rc.Width * rc.Height;
+		}
+	}
+
+	public class SquareToRectangleAdapter : IRectangle
+	{
+		Square square;
+
+		public SquareToRectangleAdapter(Square square)
+		{
+			// todo
+			this.square = square;
+		}
+
+		public int Width => square.Side;
+
+		public int Height => square.Side;
+		// todo
+	}
+}
