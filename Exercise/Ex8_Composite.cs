726a5b0e4208dea78b779d38ecdff245e1add551
Adam Jurík <ja004@seznam.cz>
2023-06-09 17:08:57 +0200

    8. Composite

---
+++ Exercise/Ex8_Composite.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace tut_DesignPatterns.Exercise
+{
+    class Ex8_Composite
+    {
+        public static void Start()
+        {
+        }
+    }
+}
+
+namespace Coding.Exercise9
+{
+    public interface IValueContainer : IEnumerable<int>
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+
+		public IEnumerator GetEnumerator()
+		{
+            yield return this;
+        }
+
+
+		IEnumerator<int> IEnumerable<int>.GetEnumerator()
+		{
+            yield return Value;
+
+        }
+    }
+
+	public class ManyValues : List<int>, IValueContainer
+	{
+
+	}
+
+	public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach(var c in containers)
+                foreach(var i in c)
+                    result += i;
+            return result;
+        }
+    }
+}
+
+
+
